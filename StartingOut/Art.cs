5f0d5c73e158b59490f996930a09efb0b7514ad8
Kyle Cook <Kyle.Cook@iam.local>
2019-05-02 20:46:28 -0500

    Fixing some issues

---
+++ StartingOut/Art.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtController
+{
+    class Art
+    {
+        public virtual void MakeArt()
+        {
+            
+        }
+    }
+
+    class SpaceArt: Art
+    {
+        public override void MakeArt()
+        {
+            
+            base.MakeArt();
+        }
+    }
+}
--- StartingOut/Assets/Scripts/GameController.cs
+++ StartingOut/Assets/Scripts/GameController.cs
@@ -2,15 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
 	#region serializedFields
 	[SerializeField]
     private Text dayNumber;
-
-    [SerializeField]
-    private Text popularityRating;
 
     [SerializeField]
     private Text moneyAmount;
@@ -42,6 +40,7 @@ public class GameController : MonoBehaviour
 	public Canvas gameplayCanvas;
 	public Canvas resultsCanvas;
 	public Text resultsText;
+    public Text billCostText;
 
 	private bool spaceIsPopular;
 	private bool characterIsPopular;
@@ -50,11 +49,11 @@ public class GameController : MonoBehaviour
 	private int daysUntilBill = 7;
     private int popularity = 0;
     private int money = 20;
-    private int supplies = 10;
+    private int supplies = 0;
 
-    private int spaceArt = 5;
-    private int characterArt = 5;
-    private int chibiArt = 5;
+    private int spaceArt = 3;
+    private int characterArt = 3;
+    private int chibiArt = 3;
 
 	private int price1;
 	private int price2;
@@ -72,6 +71,7 @@ public class GameController : MonoBehaviour
 		spaceIsPopular = true;
 		resultsCanvas.enabled = false;
 		popularArtDaily.text = "Currently popular: Space";
+        billCostText.text = "Bills cost: $130";
 		UpdatePrices();
     }
 
@@ -81,10 +81,9 @@ public class GameController : MonoBehaviour
 		UpdatePrices();
     }
 
-    private void UpdateStats()
+    private void UpdateStats() // method used to update the HUD with all the info the player needs to know.
     {
         dayNumber.text = "Days until bills: " + daysUntilBill;
-        popularityRating.text = "Popularity rating: " + popularity;
         moneyAmount.text = "Money: " + money;
         supplyAmount.text = "Supplies left: " + supplies;
     }
@@ -97,9 +96,9 @@ public class GameController : MonoBehaviour
 
     public void MakeSpaceArt()
     {
-		if(supplies >= 5)
+		if(supplies >= 10)
 		{
-			supplies -= 5;
+			supplies -= 10;
 			spaceArt++;
 			spaceArtInventory.text = spaceArt.ToString();
 			spaceArtButton.interactable = true;
@@ -108,9 +107,9 @@ public class GameController : MonoBehaviour
 
     public void MakeCharacterArt()
     {
-		if(supplies >= 3)
+		if(supplies >= 5)
 		{
-			supplies -= 3;
+			supplies -= 5;
 			characterArt++;
 			characterArtInventory.text = characterArt.ToString();
 			characterArtButton.interactable = true;
@@ -119,69 +118,69 @@ public class GameController : MonoBehaviour
 
     public void MakeChibiArt()
     {
-		if(supplies >= 1)
+		if(supplies >= 2)
 		{
-			supplies -= 1;
+			supplies -= 2;
 			chibiArt++;
 			chibiArtInventory.text = chibiArt.ToString();
 			cuteArtButton.interactable = true;
 		}
 	}
 
-	public void UpdatePrices()
+	public void UpdatePrices() // updates prices of each artwork depending on the popular subject of the day.
 	{
-		price1 = 30;
+		price1 = 25;
 		priceText1.text = "$" + price1.ToString();
 		price2 = 15;
 		priceText2.text = "$" + price2.ToString();
-		price3 = 3;
+		price3 = 10;
 		priceText3.text = "$" + price3.ToString();
 
 		if (spaceIsPopular == true)
 		{
-			price1 = 45;
+			price1 = 40;
 			priceText1.text = "$" + price1.ToString();
 			price2 = 15;
 			priceText2.text = "$" + price2.ToString();
-			price3 = 3;
+			price3 = 10;
 			priceText3.text = "$" + price3.ToString();
 		}
 		if (characterIsPopular == true)
 		{
-			price1 = 30;
+			price1 = 25;
 			priceText1.text = "$" + price1.ToString();
 			price2 = 25;
 			priceText2.text = "$" + price2.ToString();
-			price3 = 3;
+			price3 = 10;
 			priceText3.text = "$" + price3.ToString();
 		}
 		if (cuteIsPopular == true)
 		{
-			price1 = 30;
+			price1 = 25;
 			priceText1.text = "$" + price1.ToString();
 			price2 = 15;
 			priceText2.text = "$" + price2.ToString();
-			price3 = 12;
+			price3 = 25;
 			priceText3.text = "$" + price3.ToString();
 		}
 	}
 
-	private void UpdatePopularSubject()
+	private void UpdatePopularSubject() // randomizes the order of what's popular on any given day.
 	{
 		int randomizer = Random.Range(1, 3);
-		if(randomizer == 1)
+		if(randomizer == 0)
 		{
 			spaceIsPopular = true;
 			popularArtDaily.text = "Currently popular: Space";
 			UpdatePrices();
 		}
-		if (randomizer == 2)
+		if (randomizer == 1)
 		{
 			characterIsPopular = true;
 			popularArtDaily.text = "Currently popular: Character design";
 			UpdatePrices();
 		}
-		if (randomizer == 3)
+		if (randomizer == 2)
 		{
 			cuteIsPopular = true;
 			popularArtDaily.text = "Currently popular: Cute art";
@@ -235,7 +234,7 @@ public class GameController : MonoBehaviour
 		}
 	}
 
-	private IEnumerator DayPassing()
+	private IEnumerator DayPassing() // counts down a certain amount of time before counting down the day count
 	{
 		while (endCondition == false)
 		{
@@ -251,7 +250,7 @@ public class GameController : MonoBehaviour
 		}
 	}
 
-	private void EndGame()
+	private void EndGame() //the end results that occur when the 7 "days" countdown to 0.
 	{
 		gameplayCanvas.enabled = false;
 		resultsCanvas.enabled = true;
@@ -260,13 +259,18 @@ public class GameController : MonoBehaviour
 		{
 			resultsText.text = "Despite your wildest efforts, you unfortunately could not get the money you needed, and were evicted from your apartment. Maybe next time consider selling something else.";
 		}
-		if (money < 450)
+		else if (money < 450)
 		{
 			resultsText.text = "Just scraping by, you made just enough to make it through this week. Keep this pace and you should be fine.";
 		}
-		if (money > 451)
+		else if (money > 451)
 		{
 			resultsText.text = "Miraculously, your art took you off, making well more than your normal job ever could. Maybe it's time for an occupation change after all.";
 		}
 	}
+
+    public void ReturnMenuButton()
+    {
+        SceneManager.LoadScene(0);
+    }
 }
